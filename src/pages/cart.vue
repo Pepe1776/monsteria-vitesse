138231ee77267a27cd465da83b53a515a0e311e6
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-02-07 23:29:15 -0700

    added checkout

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -4,48 +4,68 @@
       Cart
     </h1>
     <div class="grid grid-cols-3 ">
-      <div class="card">
+      <div class="card remove">
         <img class="img" src="https://images.unsplash.com/photo-1614594975525-e45190c55d0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80">
         <h4 class="neon-sub text-1xl mt-2">
           Monstera Deliciosa  $55
         </h4>
         <h4>QTY:3</h4>
+        <input id="Remove" class="remove center" type="checkbox" name="Remove" label="Remove">
+        <label for="Remove">Remove</label>
       </div>
-      <div class="card grid-row-1">
+      <div class="card remove grid-row-1">
         <img class="img" src="https://images.unsplash.com/photo-1609340942215-4f9bfe38593f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80">
         <h4 class="neon-sub text-1xl mt-2">
           Anthurium Clarinervium  $45
         </h4>
         <h4>QTY:1</h4>
+        <input id="Remove" class="remove center" type="checkbox" name="Remove" label="Remove">
+        <label for="Remove">Remove</label>
       </div>
-      <div class="card grid-row-2">
+      <div class="card remove grid-row-2">
         <img class="img" src="https://images.unsplash.com/photo-1612363228106-c94b0754e2c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80">
         <h4 class="neon-sub text-1xl mt-2">
           Mini Monstera  $30
         </h4>
         <h4>QTY:2</h4>
+        <input id="Remove" class="remove center" type="checkbox" name="Remove" label="Remove">
+        <label for="Remove">Remove</label>
       </div>
-      <div class="card">
+      <div class="card remove">
         <img class="img" src="https://images.unsplash.com/photo-1611211232932-da3113c5b960?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80">
         <h4 class="neon-sub text-1xl mt-2">
           Snake Plant  $50
         </h4>
         <h4>QTY:4</h4>
+        <input id="Remove" class="remove center" type="checkbox" name="Remove" label="Remove">
+        <label for="Remove">Remove</label>
       </div>
-      <div class="card">
+      <div class="card remove">
         <img class="img" src="https://images.unsplash.com/photo-1603436326446-74e2d65f3168?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=471&q=80">
         <h4 class="neon-sub text-1xl mt-2">
           Caldium  $30
         </h4>
+        <h4>QTY:4</h4>
+        <input id="Remove" class="remove center" type="checkbox" name="Remove" label="Remove">
+        <label for="Remove">Remove</label>
       </div>
     </div>
-    <button class="btn">
+    <router-link to="/checkout" class="btn">
       Continue to Checkout
-    </button>
+    </router-link>
   </div>
 </template>
-<script setup lang="ts">
+<script lang="ts">
 </script>
-<style>
-
+<style scoped>
+.remove:checked {
+    display: none;
+}
+.remove:active {
+    display: none;
+}
+.img {
+  width:100px;
+  height: 100px;
+  }
 </style>
